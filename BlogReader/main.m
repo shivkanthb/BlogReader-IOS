92eab436d6b535621e7a4b1c69af5e6b88c8114b
shivkanth <shivkanthb@gmail.com>
2014-05-16 23:12:37 +0530

    Initial Commit

---
+++ BlogReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlogReader
+//
+//  Created by shivkanth on 16/05/14.
+//  Copyright (c) 2014 shivkanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ndAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ndAppDelegate class]));
+    }
+}
---
+++ BlogReader/ndAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ndAppDelegate.h
+//  BlogReader
+//
+//  Created by shivkanth on 16/05/14.
+//  Copyright (c) 2014 shivkanth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ndAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
